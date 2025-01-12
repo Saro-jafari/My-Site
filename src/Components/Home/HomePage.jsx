4b9c39d39906dad217dfa195c65832267520be0d
saro jafari <sarojafari2004@gmail.com>
2025-01-12 12:49:25 +0330

    add pakage.json

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { PiTelegramLogoLight } from 'react-icons/pi';
 import { RxGithubLogo } from 'react-icons/rx';
 import { FaLinkedinIn } from 'react-icons/fa';
@@ -11,13 +12,17 @@ const HomePage = () => {
 	return (
 		<>
 			<main className="relative flex flex-col-reverse md:flex-row md:justify-around md:items-center h-screen md:h-[70vh]">
+				{/* Mobile Background Image */}
 				<section
 					className="absolute top-0 left-0 w-full h-full bg-cover bg-center bg-no-repeat md:hidden"
-					style={{ backgroundImage: `url(${ImageProfile.src})`, objectFit: 'cover' }}></section>
+					style={{
+						backgroundImage: `url(${ImageProfile.src})`,
+						objectFit: 'cover',
+					}}></section>
 
+				{/* Content Section */}
 				<section className="flex flex-col md:justify-end justify-center md:gap-y-2 text-white md:text-black md:p-0 z-10 md:w-1/2 md:static absolute top-[250px] right-4 md:mt-auto">
 					<h1 className="text-[35px] md:text-[47px] md:font-semibold font-extrabold md:leading-[48px] text-[#dddd]">
-						{' '}
 						ساروجعفری&#8204;هستم
 					</h1>
 					<p className="text-[35px] md:text-[98px] font-bold leading-[60px] text-[#dddd] my-[15px] ">برنامه نویس</p>
@@ -32,17 +37,17 @@ const HomePage = () => {
 						<section className="flex gap-5 ">
 							<a
 								href="https://t.me/Sarojafarii"
-								className="px-2 py-2  bg-transparent text-white md:border-[#8750f7] border-[2px] rounded-full hover:bg-[#8750F7] cursor-pointer">
+								className="px-2 py-2 bg-transparent text-white md:border-[#8750f7] border-[2px] rounded-full hover:bg-[#8750F7] cursor-pointer">
 								<PiTelegramLogoLight size={25} />
 							</a>
 							<a
 								href="https://github.com/Saro-jafari"
-								className="px-2 py-2  bg-transparent text-white md:border-[#8750f7] border-[2px] rounded-full hover:bg-[#8750F7] cursor-pointer">
+								className="px-2 py-2 bg-transparent text-white md:border-[#8750f7] border-[2px] rounded-full hover:bg-[#8750F7] cursor-pointer">
 								<RxGithubLogo size={25} />
 							</a>
 							<a
 								href="https://www.linkedin.com/in/saro-jafari-30a787243/"
-								className="px-2 py-2  bg-transparent text-white md:border-[#8750f7] border-[2px] rounded-full hover:bg-[#8750F7] cursor-pointer">
+								className="px-2 py-2 bg-transparent text-white md:border-[#8750f7] border-[2px] rounded-full hover:bg-[#8750F7] cursor-pointer">
 								<FaLinkedinIn size={25} />
 							</a>
 						</section>
@@ -58,11 +63,13 @@ const HomePage = () => {
 
 				{/* Desktop Image */}
 				<section className="hidden md:block">
-					<Image src={ImageProfile} alt="saro" width={300} height={300} className="shadow-md rounded-lg object-cover" />
+					<Image priority src={ImageProfile} alt="saro" width={300} height={300} className="shadow-md rounded-lg object-cover" />
 				</section>
 			</main>
+
+			{/* Skills Section */}
 			<section className="flex justify-center pt-28">
-				<MySkills />{' '}
+				<MySkills />
 			</section>
 		</>
 	);
